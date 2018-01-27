d87720ea7c8940f6a54c6114f9d3837a2bada979
HaleyAlexndria <haleyplott@yahoo.com>
2018-01-26 22:22:24 -0500

    Login/signup page

---
+++ app/src/main/java/jan/pitt/si/Login.java
@@ -0,0 +1,20 @@
+package jan.pitt.si;
+
+/**
+ * Created by haleyplott on 1/26/18.
+ */
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;package jan.pitt.si;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
